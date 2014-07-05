c22977de4f7c6c2cb18c0bc66fec20cee29af8b4
T107 <T107@PC253>
2014-07-04 21:38:59 -0500

    examen

---
+++ src/main/java/com/mycompany/examen/Evaluacion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.examen;
+
+/**
+ *
+ * @author T107
+ */
+public class Evaluacion {
+    
+    public static String nombre;
+    public static float calificacion;
+    
+    public Evaluacion(String nombre, float calificacion) {
+        this.nombre = nombre;
+        this.calificacion = calificacion;
+    }
+    public class nombre {
+        private String nombre;
+        return nombre
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre
+    
+   public class calificacion {
+       private float calificacion;
+       return calificacion
+   } 
+    
+}
+
+   
+    
+}
